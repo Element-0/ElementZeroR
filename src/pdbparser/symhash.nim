6b6662f0e06ed0137ea8a9d243c5d1cada98de4b
CodeHz <codehz@outlook.com>
2020-12-21 17:09:31 +0800

    fix int overflow

--- src/pdbparser/symhash.nim
+++ src/pdbparser/symhash.nim
@@ -1,8 +1,6 @@
-{.push overflowChecks:off.}
 proc symhash*(symbol: string): int64 =
   const fnv_prime = 1099511628211
   result = cast[int64](14695981039346656037u64)
   for ch in symbol:
-    result = result * fnv_prime
+    result = result *% fnv_prime
     result = result xor int64(ch)
-{.pop.}
