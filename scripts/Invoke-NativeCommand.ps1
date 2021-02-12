dda95e7d31df97489983ddf7f5a12eea9074c616
CodeHz <codehz@outlook.com>
2021-02-12 23:30:06 +0800

    fix Invoke-NativeCommand

--- scripts/Invoke-NativeCommand.ps1
+++ scripts/Invoke-NativeCommand.ps1
@@ -1,57 +1,34 @@
 function Invoke-NativeCommand {
-    <#
-    .SYNOPSIS
-        Invoke a native command (.exe) as a new process.
-
-    .DESCRIPTION
-        Invoke-NativeCommand executes an arbitrary executable as a new process. Both the standard
-        and error output streams are redirected.
-
-        Error out is written as a single non-terminating error. ErrorAction can be used to raise
-        this as a terminating error.
-
-    .EXAMPLE
-        Invoke-NativeCommand git clone repo-uri -ErrorAction "Stop"
-
-        Run the git command to clone repo-uri. Raise a terminating error if the command fails.
-    #>
-
-    [CmdletBinding()]
-    param (
-        <#
-            The command line to execute. This parameter is named to attempt to avoid conflicts with
-            parameters for the executing command line.
-        #>
-        [Parameter(Position = 1, ValueFromRemainingArguments, ValueFromPipeline)]
-        $__CommandLine
+    param
+    (
+        [ScriptBlock] $ScriptBlock,
+        [string] $StderrPrefix = "",
+        [int[]] $AllowedExitCodes = @(0)
     )
 
-    process {
-        $command, $argumentList = $__CommandLine
+    $backupErrorActionPreference = $script:ErrorActionPreference
 
-        try {
-            $process = [System.Diagnostics.Process]@{
-                StartInfo = [System.Diagnostics.ProcessStartInfo]@{
-                    FileName               = (Get-Command $command -ErrorAction "Stop").Source
-                    Arguments              = $argumentList
-                    WorkingDirectory       = $pwd
-                    RedirectStandardOutput = $true
-                    RedirectStandardError  = $true
-                    UseShellExecute        = $false
+    $script:ErrorActionPreference = "Continue"
+    try
+    {
+        & $ScriptBlock 2>&1 | ForEach-Object -Process `
+            {
+                if ($_ -is [System.Management.Automation.ErrorRecord])
+                {
+                    "$StderrPrefix$_"
+                }
+                else
+                {
+                    "$_"
                 }
             }
-            $null = $process.Start()
-            $process.WaitForExit()
-
-            while (-not $process.StandardOutput.EndOfStream) {
-                $process.StandardOutput.ReadToEnd()
-            }
-
-            while (-not $process.StandardError.EndOfStream) {
-                Write-Error $process.StandardError.ReadToEnd()
-            }
-        } catch {
-            Write-Error -ErrorRecord $_
+        if ($AllowedExitCodes -notcontains $LASTEXITCODE)
+        {
+            throw "Execution failed with exit code $LASTEXITCODE"
         }
+    }
+    finally
+    {
+        $script:ErrorActionPreference = $backupErrorActionPreference
     }
 }
